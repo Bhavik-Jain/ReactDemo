f5aec80fac2a79daeb63fe6e7967e64572df03f1
Bhavik Jain <bhvaikjain@gmail.com>
2021-07-18 13:09:59 +0530

    React form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,81 @@
 import React, { useState } from 'react';
 
+function App(){
+
+  const [name, setName] = useState();
+  const [fname, setFname] = useState();
+
+  function inputEvent(e){
+    // console.log(e.target.value);
+
+    setName(e.target.value);
+  }
+
+  function onSubmit(){
+    setFname(name);
+  }
+
+  return(
+    <>
+      <div>
+        <h1> Hello { fname } </h1>
+        <input 
+          type="text" 
+          placeholder='Enter Your Name' 
+          onChange={inputEvent}
+          value={name}
+          />
+        <button onClick={onSubmit}> Click Me</button>
+      </div>
+    </>
+  );
+}
+
+/* ------ Event Handling ------ */
+
+// function App(){
+//   const color = '#fff';
+//   const [bg, setBg] = useState(color);
+//   const [name, setName] = useState('change 🔥');
+
+//   function UpdateBg(){
+//     let newBg = "#34495e"; 
+//     setBg(newBg);
+//     setName("change 🔥");
+//   }
+
+//   function prevBg(){
+//     setBg(color);
+//   }
+
+//   return(
+//     <>
+//       <div style={{ backgroundColor: bg }}>
+//         <button onMouseEnter={UpdateBg} onMouseLeave={prevBg}> {name} </button>
+//       </div>
+//     </>
+//   );
+
+// }
+
+
 
 /* ------ Digital Clock ------ */
 
-function App(){
+// function App(){
 
-  let time = new Date().toLocaleTimeString();
+//   let time = new Date().toLocaleTimeString();
 
-  const [ctime, setTime] = useState(time);
+//   const [ctime, setTime] = useState(time);
 
-  function UpdateTime(){
-    time = new Date().toLocaleTimeString();
-    setTime(time);
-  }
-  setInterval(UpdateTime, 1000);
+//   function UpdateTime(){
+//     time = new Date().toLocaleTimeString();
+//     setTime(time);
+//   }
+//   setInterval(UpdateTime, 1000);
 
-  return <h1>  {ctime} </h1>;
-}
+//   return <h1>  {ctime} </h1>;
+// }
 
 
 /* ------ Get Time ------ */
