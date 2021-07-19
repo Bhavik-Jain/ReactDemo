9bd85b866d0b38d2305a6277d2084e0e8bd349db
Bhavik Jain <bhvaikjain@gmail.com>
2021-07-19 16:11:26 +0530

    new updates

--- src/App.js
+++ src/App.js
@@ -1,34 +1,119 @@
 import React, { useState } from 'react';
 
-function App(){
+/* ------ Submit Form Data ------ */
 
-  const [name, setName] = useState();
-  const [fname, setFname] = useState();
+function App(){
+  const [data, setData] = useState({
+    fname: '',
+    lname : '',
+    email : ''
+  });
 
   function inputEvent(e){
-    // console.log(e.target.value);
-    setName(e.target.value);
+    console.log(e.target.value);
+    console.log(e.target.name);
+
+    // const value = e.target.value;
+    // const name = e.target.name;
+
+    const {name, value} = e.target;
+    
+    setData(function (preValue){
+
+      return{
+        ...preValue,
+        name: value,
+      }
+      // if(name === 'Fname'){
+      //   return{
+      //     fname: value,
+      //     lname: preValue.lname,
+      //   }  
+      // }else if(name === 'Lname'){
+      //   return{
+      //     fname: preValue.fname,
+      //     lname: value,
+      //   }  
+      // }
+    });
   }
 
-  function onSubmit(){
-    setFname(name);
-  }
+  function onSubmit(e){
+    e.preventDefault();
+    alert('form submitted');
+  } 
 
   return(
     <>
-      <div>
-        <h1> Hello { fname } </h1>
-        <input 
-          type="text" 
-          placeholder='Enter Your Name' 
-          onChange={inputEvent}
-          value={name}
-          />
-        <button onClick={onSubmit}> Click Me</button>
+      <div className="main_div">
+        <form onSubmit={onSubmit}>
+          <div>
+            <h1>Hello {data.fname} {data.lname} </h1>
+            <br />
+            <h6> {data.email} </h6>
+            <br />
+            <input 
+              type="text" 
+              placeholder='Enter Your Name' 
+              name = 'Fname'
+              onChange={inputEvent}
+              value={data.fname}
+            />
+            <br />
+            <input 
+              type="text" 
+              placeholder='Enter Your Last Name' 
+              name = 'Lname'
+              onChange={inputEvent}
+              value={data.lname}
+            />
+            <br />
+            <input 
+              type="email" 
+              placeholder='Enter Your mail'
+              name="email" 
+              onChange={inputEvent}
+              value={data.email}
+            />
+            <br />
+            <button type="submit"> Click Me </button>
+          </div>
+        </form>
       </div>
     </>
   );
 }
+
+/* ------ Forms ------ */
+
+// function App(){
+//   const [name, setName] = useState();
+//   const [fname, setFname] = useState();
+
+//   function inputEvent(e){
+//     // console.log(e.target.value);
+//     setName(e.target.value);
+//   }
+
+//   function onSubmit(){
+//     setFname(name);
+//   }
+
+//   return(
+//     <>
+//       <div>
+//         <h1> Hello { fname } </h1>
+//         <input 
+//           type="text" 
+//           placeholder='Enter Your Name' 
+//           onChange={inputEvent}
+//           value={name}
+//           />
+//         <button onClick={onSubmit}> Click Me</button>
+//       </div>
+//     </>
+//   );
+// }
 
 /* ------ Event Handling ------ */
 
